403cecfe261956f66586a4d2e6a2a1df284b8a48
Christian Broberg <cb@webhouse.dk>
2017-09-04 23:27:26 +0200

    Keep trying

--- src/components/Heading/Heading.js
+++ src/components/Heading/Heading.js
@@ -1,17 +1,17 @@
 import React, { Component } from 'react'
 import PropTypes from 'prop-types'
-import { StyledHeading1, StyledHeading2, StyledHeading3, StyledHeading4, StyledHeading5, StyledHeading6 } from './StyledHeading'
+import { StyledHeading1/* , StyledHeading2, StyledHeading3, StyledHeading4, StyledHeading5, StyledHeading6 */ } from './StyledHeading'
 
 class Heading extends Component {
 	render() {
 		return (
 			<div>
 				<StyledHeading1>{this.props.children}</StyledHeading1>
-				<StyledHeading2>{this.props.children}</StyledHeading2>
+				{/* <StyledHeading2>{this.props.children}</StyledHeading2>
 				<StyledHeading3>{this.props.children}</StyledHeading3>
 				<StyledHeading4>{this.props.children}</StyledHeading4>
 				<StyledHeading5>{this.props.children}</StyledHeading5>
-				<StyledHeading6>{this.props.children}</StyledHeading6>
+				<StyledHeading6>{this.props.children}</StyledHeading6> */}
 			</div>
 		)
 	}
