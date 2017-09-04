29905dc84964bac1695bfbb508fcb5dddc9852be
Christian Broberg <cb@webhouse.dk>
2017-09-05 00:54:32 +0200

    Updating deployment specs ...

--- example/src/index.js
+++ example/src/index.js
@@ -2,4 +2,4 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import App from './App'
 
-ReactDOM.render(<App name="Christian"/>, document.getElementById('root'))
+ReactDOM.render(<App name="ODEUM Primitives"/>, document.getElementById('root'))
